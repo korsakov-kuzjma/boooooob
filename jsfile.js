406ad8c1d9ebbc80bf2852a4b348924495b83e37
Kuzjma Korsakov <korsakov.kuzjma@gmail.com>
2016-12-05 12:56:07 +0800

    4

--- jsfile.js
+++ jsfile.js
@@ -1,3 +1,8 @@
 /**
  * Created by Kuzjma on 05.12.2016.
  */
+$(document).ready(function(){
+
+    $("#content").html('Lrem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam commodi dolorem eaque esse facere fugiat harum, iusto maiores maxime obcaecati odit quas repellat veniam! Ad architecto autem eaque eligendi error ex minus quibusdam sint sunt tenetur. Enim possimus quas repellendus sapiente sint voluptate! Accusantium dolorum error est fuga impedit mollitia neque sed. Accusantium amet aperiam at beatae corporis dolore doloremque dolorum ea earum eos error fugit hic id minima necessitatibus neque nulla pariatur quae quam quibusdam, quisquam quos ratione reiciendis rem repudiandae rerum temporibus! Accusantium amet dignissimos eaque eum eveniet inventore laboriosam laborum laudantium magnam magni, maiores repellat sint vero.');
+
+});
